a498146694676139f8fe0a4a6d99de11170e8b28
caijf <369756941@qq.com>
2023-11-23 16:08:09 +0800

    chore: update deps

--- examples/mst/src/utils/storage.ts
+++ examples/mst/src/utils/storage.ts
@@ -1,15 +1,15 @@
-import Cache2 from 'cache2';
+import { Cache } from 'cache2';
 
 // 本地存储命名空间，建议改为项目名
 const STORE_NAMESPACE = 'cra-template-doly';
-const cache = new Cache2(STORE_NAMESPACE, {
+const cache = new Cache(STORE_NAMESPACE, {
   storage: window.sessionStorage
 });
 
 // 登录信息
 const LOGIN_INFO_KEY = 'loginInfo';
 
-export const getLoginInfo = () => cache.get(LOGIN_INFO_KEY) as (API.Login['data'] | undefined);
+export const getLoginInfo = () => cache.get(LOGIN_INFO_KEY) as API.Login['data'] | undefined;
 export const setLoginInfo = (data: API.Login['data']) => {
   cache.set(LOGIN_INFO_KEY, data);
 };
